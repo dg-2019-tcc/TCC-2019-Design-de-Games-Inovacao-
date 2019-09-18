44c8816535f6a5778cf33856e1fd1018bc36ee58
Khalil Yamada Gangeme <khalil.yamada@hotmail.com>
2019-09-18 04:03:07 -0300

    Organizando coletaveis 2D

---
+++ Design de Games - Inovação/Assets/_Script/DestroyColetavel2D.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyColetavel2D : MonoBehaviourPunCallbacks
+{
+    PhotonView playerView;
+
+    private void OnTriggerEnter2D(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerView = other.GetComponent<PhotonView>();
+            if(playerView.IsMine == true && playerView.IsMine != null)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
