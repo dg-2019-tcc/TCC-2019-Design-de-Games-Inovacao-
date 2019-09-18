3a68dc963628f393e88e5801f3a06e8524fe39e6
Matias <matiascodas@gmail.com>
2019-09-18 01:00:35 -0300

    Botão para sair durante o jogo

--- Design de Games - Inovação/Assets/Scripts/PauseManager.cs
+++ Design de Games - Inovação/Assets/Scripts/PauseManager.cs
@@ -1,13 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
+
+    public GameObject pausebuttons;
+
+    public void Pause()
+    {
+        pausebuttons.SetActive(true);
+    }
 
     public void QuitGame()
     {
         Application.Quit();
     }
 
+    public void VoltaMenu()
+    {
+        SceneManager.LoadScene("DelayStartMenuDemo");
+    }
+
+    public void VoltaJogo()
+    {
+        pausebuttons.SetActive(false);
+    }
 }
