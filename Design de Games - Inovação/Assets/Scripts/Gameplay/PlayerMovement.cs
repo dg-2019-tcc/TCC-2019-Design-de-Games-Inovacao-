8e4a4443f4c5679867ff052e536509703cf806a3
Matias <matiascodas@gmail.com>
2019-10-21 13:09:06 -0300

    Novo modelo, sem animaçoes e sem a troca de customização

---
+++ Design de Games - Inovação/Assets/Scripts/Gameplay/PlayerMovement.cs
@@ -0,0 +1,307 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun.UtilityScripts;
+using Cinemachine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float numeroDeColetaveis;
+
+    [HideInInspector]
+    public float coletavel;
+
+
+    public float speed;
+    public float jumpSpeed;
+    public float maxSpeed = 8;
+    private Rigidbody2D rb2d;
+    public bool jump;
+
+
+    // private Vector3 oldPosition;
+
+    public GameObject player;
+
+
+    protected Joystick joyStick;
+    protected FixedButton fixedButton;
+    public GameObject canvasSelf;
+
+
+    public Transform groundCheck;
+    public bool grounded;
+
+
+    public GameObject Pet;
+
+
+    public bool pipa;
+    public float pipaForce;
+    public GameObject pipaObj;
+
+
+    public bool carrinho;
+    public float carrinhoSpeed;
+    public GameObject carrinhoObj;
+
+    [HideInInspector]
+    public PhotonView PV;
+    private CinemachineConfiner CC;
+    private CinemachineVirtualCamera VC;
+
+    public GameObject dogSpawn;
+    public float dogCount;
+
+    public bool desativa;
+
+    public AudioSource puloSom;
+    public GameObject walkSom;
+    public AudioSource tokenSom;
+    public AudioSource coleta;
+
+
+
+
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+
+        joyStick = FindObjectOfType<Joystick>();
+
+        fixedButton = FindObjectOfType<FixedButton>();
+
+        PV = GetComponent<PhotonView>();
+
+        if (PV != null && PV.IsMine)
+        {
+            VC = gameObject.transform.GetChild(0).GetComponent<CinemachineVirtualCamera>();
+            VC.Priority = 15;
+
+            if (joyStick.isActiveAndEnabled)
+            {
+                CC = gameObject.transform.GetChild(0).GetComponent<CinemachineConfiner>();
+                CC.m_BoundingShape2D = GameObject.Find("CameraConfiner").GetComponent<PolygonCollider2D>();
+                CC.InvalidatePathCache();
+            }
+
+            rb2d.gravityScale = 1;
+
+        }
+        else
+        {
+            canvasSelf.SetActive(false);
+        }
+
+        if ((int)PhotonNetwork.LocalPlayer.CustomProperties["Ganhador"] == 1)
+        {
+            FindObjectOfType<Coroa>().ganhador = transform;
+            transform.position = new Vector3(0, 0, 0);
+        }
+
+
+    }
+
+
+    void FixedUpdate()
+    {
+
+
+
+
+
+
+
+        if (coletavel >= numeroDeColetaveis)
+        {
+            PhotonNetwork.LocalPlayer.CustomProperties["Ganhador"] = 1;
+
+            gameObject.GetComponent<PhotonView>().RPC("ZeraPontuacao", RpcTarget.All);
+
+            gameObject.GetComponent<PhotonView>().RPC("TrocaSala", RpcTarget.MasterClient);
+            coletavel = 0;
+
+
+        }
+
+
+        if (PV != null && !PV.IsMine) return;
+
+        //Vector2 move = new Vector2(joyStick.Horizontal + Input.GetAxisRaw("Horizontal"), 0);
+
+        if (joyStick != null)
+        {
+            if (joyStick.Horizontal > 0)
+            {
+                player.transform.rotation = Quaternion.Euler(0, 90, 0);
+
+            }
+            else if (joyStick.Horizontal < 0)
+            {
+                player.transform.rotation = Quaternion.Euler(0, -90, 0);
+            }
+        }
+
+
+        //Movimentação do player no joystick
+        float moveHorizontal = joyStick.Horizontal + Input.GetAxisRaw("Horizontal");
+
+        if (moveHorizontal != 0)
+        {
+
+            rb2d.velocity = new Vector3(speed * moveHorizontal, rb2d.velocity.y, 0);
+            //walkSom.SetActive(true);
+        }
+
+        /*if(rb2d.velocity.x > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+        }
+
+        if (rb2d.velocity.x < -maxSpeed)
+        {
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+        }*/
+
+
+        // Pulo
+        if (grounded == true && jump == true)
+        {
+            carrinho = false;
+            rb2d.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+            //Physics.IgnoreLayerCollision(10, 11, true);
+            jump = false;
+            puloSom.Play();
+
+        }
+
+        // Desativando transformações
+        if (desativa == true)
+        {
+            pipa = false;
+            carrinho = false;
+            speed = 2.5f;
+            dogCount = 0;
+            desativa = false;
+        }
+
+        //Colocando tempo limite das tranformações
+        if (dogCount >= 2f)
+        {
+            desativa = true;
+        }
+
+        //Transformação em pipa
+        if (pipa == true)
+        {
+            dogCount += Time.deltaTime;
+            rb2d.AddForce(new Vector2(0, pipaForce), ForceMode2D.Impulse);
+            pipaObj.SetActive(true);
+            carrinho = false;
+            maxSpeed = 4;
+        }
+
+        if (pipa == false && !Pet.activeSelf)
+        {
+            pipaObj.SetActive(false);
+            Pet.transform.position = dogSpawn.transform.position;
+        }
+
+        // Transformação em carrinho
+        if (carrinho == true && jump == false)
+        {
+            dogCount += Time.deltaTime;
+            //rb2d.AddForce(-Vector2.up * carrinhoSpeed);
+            speed = carrinhoSpeed;
+            carrinhoObj.SetActive(true);
+            pipa = false;
+        }
+
+        if (carrinho == false && !Pet.activeSelf)
+        {
+            carrinhoObj.SetActive(false);
+
+        }
+
+
+        if (carrinho == false && pipa == false)
+        {
+            TransformaPet(true, "carrinho");
+
+
+        }
+
+        else
+        {
+            TransformaPet(false, "carrinho");
+            Pet.transform.position = dogSpawn.transform.position;
+        }
+
+
+
+
+        //Debug.Log(rb2d.velocity);
+    }
+
+
+
+
+
+    [PunRPC]
+    void TrocaSala()
+    {
+        PhotonNetwork.LoadLevel("TelaVitoria");
+    }
+
+    [PunRPC]
+    void ZeraPontuacao()
+    {
+        PV.Owner.SetScore(0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Coletavel"))
+        {
+            if (PV.IsMine == true)
+            {
+                coletavel++;
+                coleta.Play();
+            }
+        }
+
+        if (collision.CompareTag("Pipa"))
+        {
+            pipa = true;
+            tokenSom.Play();
+        }
+
+        if (collision.CompareTag("Carrinho"))
+        {
+            carrinho = true;
+            tokenSom.Play();
+        }
+    }
+
+
+    private void TransformaPet(bool isDog, string transformation)
+    {
+        Pet.SetActive(isDog);
+    }
+
+
+    //Função para o botão de pulo
+    public void Jump()
+    {
+        jump = true;
+        if (pipa == true || carrinho == true)
+        {
+            desativa = true;
+        }
+
+    }
+}
