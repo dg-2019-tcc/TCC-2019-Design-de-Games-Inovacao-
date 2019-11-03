326d4a017368aea61bc4431aff02bff6e87cd6b8
César Neves Mosca <Cronokami@hotmail.com>
2019-11-02 21:53:32 -0300

    Stickers Online
    Supostamente

--- Design de Games - Inovação/Assets/Scripts/Gameplay/EmotesManager.cs
+++ Design de Games - Inovação/Assets/Scripts/Gameplay/EmotesManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TouchScript.Gestures;
+using Photon.Pun;
 
 public class EmotesManager : MonoBehaviour
 {
@@ -12,6 +13,14 @@ public class EmotesManager : MonoBehaviour
 	public void Sticker(int index)
 	{
 		pause.SetActive(false);
+		gameObject.GetComponent<PhotonView>().RPC("MandaSticker", RpcTarget.All, index);
+
+	}
+
+	[PunRPC]
+	private void MandaSticker(int index)
+	{
+
 		emote[index].SetActive(true);
 
 	}
