cae456bef866a0df5c5b3adc145863b8b155cdda
Khalil Yamada Gangeme <khalil.yamada@hotmail.com>
2019-11-06 01:43:57 -0300

    Estrutura do tutorial Finalizada

---
+++ Design de Games - Inovação/Assets/Scripts/Tutorial/FinalizaTutorial.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+
+public class FinalizaTutorial : MonoBehaviour
+{
+    [Header("Tela de vitória")]
+    [SerializeField]
+    private string victoryLevel;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PhotonNetwork.LoadLevel(victoryLevel);
+        }
+    }
+}
