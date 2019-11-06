70ca44be36938a9d9d6d2a3baae5ff56632893f8
Matias <matiascodas@gmail.com>
2019-11-06 03:01:53 -0300

    groundcheck

--- Design de Games - Inovação/Assets/Scripts/Gameplay/GroundCheck.cs
+++ Design de Games - Inovação/Assets/Scripts/Gameplay/GroundCheck.cs
@@ -23,8 +23,9 @@ public class GroundCheck : MonoBehaviour
     void OnTriggerEnter2D(Collider2D col)
     {
         if (!col.CompareTag("Coletavel"))
-        player.grounded = true;
+        
         playerAC.SetTrigger("Fall");
+        player.grounded = true;
     }
 
      void OnTriggerStay2D(Collider2D col)
