5a120da3ee32fffc29af5e4c850436ebc82941ed
Matias <matiascodas@gmail.com>
2019-11-05 23:18:20 -0300

    Plataforma agua e dragão na fase

---
+++ Design de Games - Inovação/Assets/PlataformaAgua.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaAgua : MonoBehaviour
+{
+    public Transform target;
+    public Transform startPos;
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        if (Vector3.Distance(transform.position, target.position) < 0.001f)
+        {
+            transform.position = startPos.transform.position;
+        }
+    }
+}
