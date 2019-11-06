5a94e40d3e4a881dc3c953d60cbba63f0a7ac8ad
Matias <matiascodas@gmail.com>
2019-11-05 22:09:46 -0300

    Auto stash before merge of "CustomCorreta" and "origin/CustomCorreta"

---
+++ Design de Games - Inovação/Assets/AtivaDragao.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtivaDragao : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Dragao"))
+        {
+            other.GetComponent<SpriteRenderer>().enabled = true;
+            other.gameObject.layer = 0;
+        }
+    }
+}
---
+++ Design de Games - Inovação/Assets/DesativaDragao.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesativaDragao : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Dragao"))
+        {
+            Debug.Log("Desativou");
+            other.GetComponent<SpriteRenderer>().enabled = false;
+            other.gameObject.layer = 11;
+            
+        }
+    }
+}
