0a58615b89e612c484adc10dcb6a326de6ca4894
César Neves Mosca <Cronokami@hotmail.com>
2019-11-11 16:00:51 -0300

    Código para animações de rosto
    e delay de players testado

---
+++ Design de Games - Inovação/Assets/Scripts/Gameplay/PlayerFaceAnimations.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerFaceAnimations : MonoBehaviour
+{
+	public SkinnedMeshRenderer playerMesh;
+	public Texture[] textures;
+
+	[Header("Index for animation")]
+	public int face;
+
+	private int lastFace;
+
+	private PhotonView pv;
+
+
+	private void Start()
+	{
+		pv = GetComponent<PhotonView>();
+	}
+
+	void Update()
+	{
+		if (pv.IsMine && face != lastFace)
+		{
+			pv.RPC("TrocaCara", RpcTarget.All, face);
+		}
+
+		lastFace = face;
+	}
+
+	[PunRPC]
+	void TrocaCara(int index)
+	{
+		playerMesh.materials[0].mainTexture = textures[index];
+	}
+}
