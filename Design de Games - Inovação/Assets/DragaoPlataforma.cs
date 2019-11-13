c5dc2d1f9bcea749803a96554f19d572912d8449
Matias <matiascodas@gmail.com>
2019-11-13 15:07:56 -0300

    Tentando Arrumar o dragão

---
+++ Design de Games - Inovação/Assets/DragaoPlataforma.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragaoPlataforma : MonoBehaviour
+{
+    private Transform target;
+    public float speed = 1.0f;
+
+
+    private void Start()
+    {
+        target = transform;
+    }
+
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = transform.parent;
+        }
+    }
+}
