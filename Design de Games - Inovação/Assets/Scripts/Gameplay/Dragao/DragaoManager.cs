95713682b09ef6e3676d2ef9fd226047088da5cf
Khalil Yamada Gangeme <khalil.yamada@hotmail.com>
2019-11-14 00:13:08 -0300

    Arrumando o dragão

---
+++ Design de Games - Inovação/Assets/Scripts/Gameplay/Dragao/DragaoManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragaoManager : MonoBehaviour
+{
+
+    [Header("Prefab")]
+
+    [SerializeField]
+    private GameObject nodeDoDragao;
+    private GameObject nomeDoNodeDoDragao;
+    [SerializeField]
+    private Transform parentDragao;
+
+
+    [Header("Variaveis")]
+
+    private float numeroDeNodes;
+    private float posicao;
+    [SerializeField]
+    private float angulo;
+
+
+    private void Start()
+    {
+        InstantiateDragao();
+    }
+
+
+
+    private void Update()
+    {
+        
+    }
+
+
+
+    void InstantiateDragao()
+    {
+        numeroDeNodes = 360 / angulo;
+        for (int i = 0; i < numeroDeNodes; i++)
+        {
+            posicao = angulo * i;
+            nomeDoNodeDoDragao = Instantiate(nodeDoDragao, transform.position, Quaternion.Euler(0, 0, posicao), parentDragao);
+            if (i <= 9)
+            {
+                nomeDoNodeDoDragao.name = "Node_00" + i;
+            }
+            else
+            {
+                nomeDoNodeDoDragao.name = "Node_0" + i;
+            }
+        }
+    }
+    
+
+
+
+    void MoveDragao()
+    {
+
+    }
+
+}
---
+++ Design de Games - Inovação/Assets/Scripts/Gameplay/Dragao/DragaoSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragaoSwitch : MonoBehaviour
+{
+    [Header ("Switch")]
+
+    [SerializeField]
+    private bool switchDragao;
+    [SerializeField]
+    private bool layerDragao;
+
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Dragao"))
+        {
+            other.GetComponent<SpriteRenderer>().enabled = switchDragao;
+            other.gameObject.layer = layerDragao;
+        }
+    }
+}
