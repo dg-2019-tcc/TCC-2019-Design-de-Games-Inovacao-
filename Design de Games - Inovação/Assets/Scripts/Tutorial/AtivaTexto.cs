512acfbdf2a94f69a8809aeeaaa406d68bfc468f
Khalil Yamada Gangeme <khalil.yamada@hotmail.com>
2019-11-22 18:55:23 -0300

    Criando Script que ativa o texto no tutorial
    Criei um script que pode ser customizável e que vai ativar uma caixa de texto na cabeça do jogador

---
+++ Design de Games - Inovação/Assets/Scripts/Tutorial/AtivaTexto.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using Photon.Pun;
+
+public class AtivaTexto : MonoBehaviour
+{
+
+    [Header("EscolheImagem")]
+
+    [SerializeField]
+    private Sprite fala;
+    [SerializeField]
+    private GameObject falaGO;
+
+
+
+    [Header("Variaveis")]
+
+    static bool falaAtiva;
+
+
+
+    private void Update()
+    {/*
+
+        
+
+        if (falaAtiva)
+        {
+            StartCoroutine(IniciaFala());
+       }*/
+    }
+
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if(falaAtiva == false)
+            {
+                falaAtiva = true;
+                GameObject entry = Instantiate(falaGO);
+                entry.transform.SetParent(collision.transform);
+                entry.GetComponentInChildren<Image>().sprite = fala;
+            }            
+        }
+    }
+
+
+
+    private IEnumerator IniciaFala()
+    {
+        falaAtiva = false;        
+        falaGO.SetActive(true);
+        yield return new WaitForSeconds(4);
+        falaGO.SetActive(false);
+    }
+}
