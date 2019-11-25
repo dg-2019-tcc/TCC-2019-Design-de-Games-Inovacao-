667d907a62a8e4b7c9bc0cc57defa164a53d8e14
César Neves Mosca <Cronokami@hotmail.com>
2019-11-25 17:38:57 -0300

    Créditos

---
+++ Design de Games - Inovação/Assets/Scripts/Interface/Menus/CreditosManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditosManager : MonoBehaviour
+{
+	public string nomeDoMenu;
+	public void VoltaMenu()
+	{
+		SceneManager.LoadScene(nomeDoMenu);
+	}
+}
--- Design de Games - Inovação/Assets/Scripts/Interface/Menus/MenuPrincipal.cs
+++ Design de Games - Inovação/Assets/Scripts/Interface/Menus/MenuPrincipal.cs
@@ -7,9 +7,16 @@ public class MenuPrincipal : MonoBehaviour
 {
     [SerializeField]
     private string nomeDoMenu;
+	[SerializeField]
+	private string nomeDosCreditos;
 
     public void ComecaJogo()
     {
         SceneManager.LoadScene(nomeDoMenu);
     }
+
+	public void Creditos()
+	{
+		SceneManager.LoadScene(nomeDosCreditos);
+	}
 }
