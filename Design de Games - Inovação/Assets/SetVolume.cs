69d43f5b47310f2c1da3436e43f5159b6ad58883
Matias <matiascodas@gmail.com>
2019-11-25 19:49:40 -0300

    Colocando os sliders

---
+++ Design de Games - Inovação/Assets/SetVolume.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer musicMixer;
+
+    public AudioMixer SFXMixer;
+
+    public void SetLevelMusic (float sliderValue)
+    {
+        musicMixer.SetFloat("MusicVol", Mathf.Log10 (sliderValue) * 20);
+    }
+
+    public void SetLevelSFX(float sliderValue)
+    {
+        SFXMixer.SetFloat("SFXVol", Mathf.Log10(sliderValue) * 20);
+    }
+}
