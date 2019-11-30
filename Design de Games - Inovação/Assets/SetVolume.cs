d00a4cc1f73e3603f1d6b320362d8feaaecfbbe3
Matias <matiascodas@gmail.com>
2019-11-30 12:02:47 -0300

    Atualizando

--- Design de Games - Inovação/Assets/SetVolume.cs
+++ Design de Games - Inovação/Assets/SetVolume.cs
@@ -2,20 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SetVolume : MonoBehaviour
 {
     public AudioMixer musicMixer;
 
+    public FloatVariable musicVol;
+
+    public Slider musicSlider;
+
     public AudioMixer SFXMixer;
+
+    public FloatVariable sfxVol;
+
+    public Slider sfxSlider;
+
+    private void Awake()
+    {
+        musicSlider.value = musicVol.Value;
+        sfxSlider.value = sfxVol.Value;
+    }
 
     public void SetLevelMusic (float sliderValue)
     {
         musicMixer.SetFloat("MusicVol", Mathf.Log10 (sliderValue) * 20);
+        musicVol.Value = sliderValue;
     }
 
     public void SetLevelSFX(float sliderValue)
     {
         SFXMixer.SetFloat("SFXVol", Mathf.Log10(sliderValue) * 20);
+        sfxVol.Value = sliderValue;
     }
 }
