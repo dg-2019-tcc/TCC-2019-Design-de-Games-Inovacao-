c792b3312a61845bcac5f6f760625963dde9c1b7
César Neves Mosca <Cronokami@hotmail.com>
2019-11-30 21:55:35 -0300

    Camera afasta bonitinho

--- Design de Games - Inovação/Assets/Scripts/Corrida/CorridaAjustaCamera.cs
+++ Design de Games - Inovação/Assets/Scripts/Corrida/CorridaAjustaCamera.cs
@@ -9,54 +9,24 @@ using Cinemachine.Utility;
 public class CorridaAjustaCamera : MonoBehaviour
 {
 	private GameObject cam;
-	private Vector3 currentPos;
+	private Transform playerTransform;
 	private Vector3 initialPos;
-	private bool isHigh = false;
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		if (collision.CompareTag("Player") && collision.GetComponent<PhotonView>().IsMine)
+		if (collision.CompareTag("Player") && collision.GetComponent<PhotonView>().IsMine && cam == null)
 		{
-			if (cam == null)
-			{
-				cam = collision.transform.GetChild(0).gameObject;
-				initialPos = cam.transform.position;
-			}
-
-			isHigh = true;
+			cam = collision.transform.GetChild(0).gameObject;
+			initialPos = cam.transform.position;
+			playerTransform = collision.transform;
 		}
 	}
-
-	private void OnTriggerStay2D(Collider2D collision)
-	{
-		if (collision.CompareTag("Player") && collision.GetComponent<PhotonView>().IsMine)
-		{
-			currentPos = collision.transform.position;
-		}
-	}
-
-	private void OnTriggerExit2D(Collider2D collision)
-	{
-		if (collision.CompareTag("Player") && collision.GetComponent<PhotonView>().IsMine)
-		{
-			isHigh = false;
-		}
-	}
-
 
 	private void Update()
 	{
 		if (cam != null)
 		{
-			if (isHigh)
-			{
-					cam.transform.position = Vector3.Lerp(cam.transform.position, new Vector3(cam.transform.position.x, cam.transform.position.y, initialPos.z - Mathf.Clamp(currentPos.y, 0, 50)), .1f);
-				
-			}
-			else
-			{
-				cam.transform.position = Vector3.Lerp(cam.transform.position, new Vector3(cam.transform.position.x, cam.transform.position.y, initialPos.z), .1f);
-			}
+				cam.transform.position = Vector3.Lerp(cam.transform.position, new Vector3(cam.transform.position.x, cam.transform.position.y, initialPos.z - Mathf.Clamp(playerTransform.position.y, 0, 50)), .1f);
 		}
 	}
 
