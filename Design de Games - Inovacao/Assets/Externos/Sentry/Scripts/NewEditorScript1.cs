4f603811e15605952fcc007b4e9e33e4c9175ace
Matias <matiascodas@gmail.com>
2020-02-28 13:04:38 -0300

    Ajustando as animaçoes

--- Design de Games - Inovacao/Assets/Externos/Sentry/Scripts/NewEditorScript1.cs
+++ Design de Games - Inovacao/Assets/Externos/Sentry/Scripts/NewEditorScript1.cs
@@ -3,9 +3,9 @@ using UnityEditor;
 
 public class NewEditorScript1 : ScriptableObject
 {
-    [MenuItem("Tools/MyTool/Do It in C#")]
+    /*[MenuItem("Tools/MyTool/Do It in C#")]
     static void DoIt()
     {
         EditorUtility.DisplayDialog("MyTool", "Do It in C# !", "OK", "");
-    }
+    }*/
 }
