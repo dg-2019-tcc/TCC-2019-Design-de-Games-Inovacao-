7e5bdd9d5742c0fa1b5007a7b060195d87617f4f
Matias <matiascodas@gmail.com>
2020-03-02 17:00:46 -0300

    Level manager

---
+++ Design de Games - Inovacao/Assets/Scripts/ScriptablesObject/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public abstract class GameManager : MonoBehaviour
+{
+    public bool GamePaused = false;
+
+    public static GameManager Instance;
+    protected virtual void Awake()
+    {
+        #region Singleton
+
+        if (Instance)
+        {
+            Debug.Log("There is a Soccer Manager Instance already. Destroying " + this.name);
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+
+        #endregion
+    }
+
+
+    public virtual void SetPauseState(bool pauseState)
+    {
+        GamePaused = pauseState;
+        Debug.Log($"GamePaused: {GamePaused}");
+    }
+
+    public virtual void ChangePauseState()
+    {
+        SetPauseState(!GamePaused);
+    }
+}
---
+++ Design de Games - Inovacao/Assets/Scripts/ScriptablesObject/Scripts/LevelManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : GameManager
+{
+    public new static LevelManager Instance => GameManager.Instance as LevelManager;
+
+    public Level[] levels;
+    public Level CurrentLevel => levels[CurrentLevelIndex];
+    public int CurrentLevelIndex = 0;
+    public Level LastLevel => levels[levels.Length - 1];
+
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+        levels = GetLevels();
+    }
+
+    private Level GetLevels()
+    {
+
+    }
+
+
+    public void GoHub()
+    {
+
+    }
+
+    public void GoCorrida()
+    {
+
+    }
+
+    public void GoColeta()
+    {
+
+    }
+
+    public void GoPodium()
+    {
+
+    }
+}
