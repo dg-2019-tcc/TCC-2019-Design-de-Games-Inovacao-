48db16698f96cba3fd496696a1faf1637ef01e96
Matias <matiascodas@gmail.com>
2020-03-02 18:59:38 -0300

    Level Manager com as troca de cena

--- Design de Games - Inovacao/Assets/Scripts/ScriptablesObject/Scripts/LevelManager.cs
+++ Design de Games - Inovacao/Assets/Scripts/ScriptablesObject/Scripts/LevelManager.cs
@@ -1,47 +1,57 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : GameManager
 {
     public new static LevelManager Instance => GameManager.Instance as LevelManager;
 
-    public Level[] levels;
-    public Level CurrentLevel => levels[CurrentLevelIndex];
-    public int CurrentLevelIndex = 0;
-    public Level LastLevel => levels[levels.Length - 1];
+    public FloatVariable CurrentLevelIndex;
 
+    public int totalPoints;
+    
 
-
-    protected override void Awake()
-    {
-        base.Awake();
-        levels = GetLevels();
-    }
-
-    private Level GetLevels()
-    {
-
-    }
 
 
     public void GoHub()
     {
-
-    }
-
-    public void GoCorrida()
-    {
-
+        CurrentLevelIndex.Value = 0;
     }
 
     public void GoColeta()
     {
-
+        CurrentLevelIndex.Value = 1;
     }
 
+    public void GoCorrida()
+    {
+        CurrentLevelIndex.Value = 2;
+    }
+
+    [PunRPC]
+     void GoVitoria()
+    {
+        PhotonNetwork.LocalPlayer.CustomProperties["Ganhador"] = 1;
+        CurrentLevelIndex.Value = 3;
+        gameObject.GetComponent<PhotonView>().RPC("GoPodium", RpcTarget.All);
+        // Manda o jogador q ganhou pra tela como vitorioso
+        // e ativa o GoDerrota() para todos os outros
+    }
+
+    [PunRPC]
+    void GoDerrota()
+    {
+        PhotonNetwork.LocalPlayer.CustomProperties["Ganhador"] = 0;
+        //Só é ativado quando alguem ganha
+        CurrentLevelIndex.Value = 3;
+        gameObject.GetComponent<PhotonView>().RPC("GoPodium", RpcTarget.MasterClient);
+    }
+
+    [PunRPC]
     public void GoPodium()
     {
-
+        PhotonNetwork.LoadLevel("TelaVitoria");
     }
 }
--- Design de Games - Inovacao/Assets/Scripts/_Script/DestroyColetavel2D.cs
+++ Design de Games - Inovacao/Assets/Scripts/_Script/DestroyColetavel2D.cs
@@ -10,19 +10,35 @@ public class DestroyColetavel2D : MonoBehaviourPunCallbacks
     PhotonView playerView;
     public static int index;
     public AudioSource coletaSom;
-    
+
+    public FloatVariable CurrentLevelIndex;
+
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            coletaSom.Play();
-            playerView = other.GetComponent<PhotonView>();
-            if (playerView.IsMine == true)
+            if (CurrentLevelIndex.Value == 1 && index == 6)
             {
-                PlayerMovement jogador = other.GetComponent<PlayerMovement>();
-                jogador.PV.Owner.AddScore(1);
-                Destroy(gameObject);
-                index++;
+                LevelManager.Instance.GoVitoria();
+            }
+
+            else if (CurrentLevelIndex.Value == 2)
+            {
+                LevelManager.Instance.GoVitoria();
+            }
+            else
+            {
+
+                coletaSom.Play();
+                playerView = other.GetComponent<PhotonView>();
+                if (playerView.IsMine == true)
+                {
+                    PlayerMovement jogador = other.GetComponent<PlayerMovement>();
+                    jogador.PV.Owner.AddScore(1);
+                    Destroy(gameObject);
+                    index++;
+                }
             }
         }
 
