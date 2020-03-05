7ecae20057eb32982de6c287110ba81a75133ccc
César Neves Mosca <Cronokami@hotmail.com>
2020-03-05 18:34:23 -0300

    Câmera mostrando as entradas na HUB

---
+++ Design de Games - Inovacao/Assets/Scripts/SaveChecker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveChecker : MonoBehaviour
+{
+	public GameObject player;
+	public GameObject delayStartMenu;
+
+
+	private void Start()
+	{
+		//debug
+		//PlayerPrefs.SetInt("hasPlayed", 0);
+		if (PlayerPrefs.GetInt("hasPlayed") != 1){
+			player.SetActive(false);
+			delayStartMenu.SetActive(false);
+			StartCoroutine("Showoff");
+		}
+	}
+
+
+
+	private IEnumerator Showoff()
+	{
+		yield return new WaitForSeconds(10);
+		player.SetActive(true);
+		delayStartMenu.SetActive(true);
+		PlayerPrefs.SetInt("hasPlayed", 1);
+	}
+}
