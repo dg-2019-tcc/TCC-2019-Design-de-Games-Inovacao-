fac3c46078b4223bef357fcc08e77166bb29333c
Matias <matiascodas@gmail.com>
2020-03-06 16:40:44 -0300

    Cena de historia

---
+++ Design de Games - Inovacao/Assets/Scripts/Historia/HistoriaManager.cs
@@ -0,0 +1,35 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HistoriaManager : MonoBehaviour
+{
+    public Image histImage;
+    public Sprite[] histSprites;
+
+    public string nomeDoMenu;
+
+
+    private void Awake()
+    {
+        StartCoroutine("StartHist");
+    }
+
+    public void SkipHist()
+    {
+        SceneManager.LoadScene(nomeDoMenu);
+    }
+
+    IEnumerator StartHist()
+    {
+        for (int i = 0; i < histSprites.Length; i++)
+        {
+            histImage.sprite = histSprites[i];
+            yield return new WaitForSeconds(5f);
+        }
+
+        SceneManager.LoadScene(nomeDoMenu);
+    }
+}
