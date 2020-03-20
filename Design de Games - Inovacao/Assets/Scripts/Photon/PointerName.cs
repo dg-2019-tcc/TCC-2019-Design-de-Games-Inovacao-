3f8583ffbc94e5ef2f190656e48243f48769a845
César Neves Mosca <Cronokami@hotmail.com>
2020-03-19 23:07:45 -0300

    Nome aparece na bolinha

---
+++ Design de Games - Inovacao/Assets/Scripts/Photon/PointerName.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class PointerName : MonoBehaviour
+{
+
+	private TextMeshProUGUI text;
+    void Start()
+    {
+		text = GetComponent<TextMeshProUGUI>();
+		text.text = PhotonNetwork.NickName;
+    }
+	
+    void Update()
+    {
+		transform.up = Vector3.up;
+    }
+}
