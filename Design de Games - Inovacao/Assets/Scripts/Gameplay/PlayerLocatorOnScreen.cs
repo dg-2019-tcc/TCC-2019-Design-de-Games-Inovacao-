26a2416369698ac9e65e4628754a07c5f36703dc
César Neves Mosca <Cronokami@hotmail.com>
2020-03-19 22:34:14 -0300

    Bolinha mostrando onde o player está quando fora da tela

--- Design de Games - Inovacao/Assets/Scripts/Gameplay/PlayerLocatorOnScreen.cs
+++ Design de Games - Inovacao/Assets/Scripts/Gameplay/PlayerLocatorOnScreen.cs
@@ -10,6 +10,7 @@ public class PlayerLocatorOnScreen : MonoBehaviour
 	private Camera cam;
 
 	public Vector3 positionAdjust;
+	public float adjustToScreen;
     
     void Start()
     {
@@ -23,6 +24,35 @@ public class PlayerLocatorOnScreen : MonoBehaviour
     void FixedUpdate()
     {
 		instance.transform.position = cam.WorldToScreenPoint(transform.position + positionAdjust);
-		
-    }
+		if (instance.transform.position.y <= 0 || instance.transform.position.y >= Screen.height || instance.transform.position.x <= 0 || instance.transform.position.x >= Screen.width)
+		{
+			instance.SetActive(true);
+			//Vertical
+			if (instance.transform.position.y >= Screen.height)
+			{
+				instance.transform.position = new Vector3(instance.transform.position.x, Screen.height - adjustToScreen, instance.transform.position.z);
+			}
+			else if (instance.transform.position.y <= 0)
+			{
+				instance.transform.position = new Vector3(instance.transform.position.x, adjustToScreen, instance.transform.position.z);
+			}
+			//Horizontal
+			if (instance.transform.position.x >= Screen.width)
+			{
+				instance.transform.position = new Vector3(Screen.width - adjustToScreen, instance.transform.position.y, instance.transform.position.z);
+			}
+			else if (instance.transform.position.x <= 0)
+			{
+				instance.transform.position = new Vector3(adjustToScreen, instance.transform.position.y, instance.transform.position.z);
+			}
+		}
+		else
+		{
+			instance.SetActive(false);
+		}
+
+		instance.transform.up = cam.WorldToScreenPoint(transform.position + positionAdjust) - instance.transform.position;
+		//instance.transform.LookAt(transform.position);
+
+	}
 }
