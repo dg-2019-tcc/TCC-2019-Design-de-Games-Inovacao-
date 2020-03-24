dd864632a967496b1dcdc9bee40e574cd8df3d65
César Neves Mosca <Cronokami@hotmail.com>
2020-03-24 01:51:41 -0300

    Moto virando e fase se movendo

---
+++ Design de Games - Inovacao/Assets/MotoFlip.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotoFlip : MonoBehaviour
+{
+	public bool leftDir;
+   
+    void Update()
+    {
+		leftDir = PlayerMovement.leftDir;
+		if (leftDir)
+		{
+			transform.rotation = Quaternion.Euler(0, 180, 0);
+		}
+		else
+		{
+			transform.rotation = Quaternion.Euler(0, 0, 0);
+		}
+    }
+}
