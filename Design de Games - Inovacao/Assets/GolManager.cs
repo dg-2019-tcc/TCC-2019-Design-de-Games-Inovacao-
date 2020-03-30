944a82c430fb05680f1c7da4788e98fc7a60afa0
Matias <matiascodas@gmail.com>
2020-03-30 19:20:38 -0300

    associando o gol ao player

--- Design de Games - Inovacao/Assets/GolManager.cs
+++ Design de Games - Inovacao/Assets/GolManager.cs
@@ -8,31 +8,33 @@ using UnityEngine;
 public class GolManager : MonoBehaviourPunCallbacks
 {
     PhotonView playerView;
-    public static int index;
-    [HideInInspector]
-    public static float coletavel;
-    public AudioSource coletaSom;
 
+    public  int index;
 
+    public GameObject bola;
 
-    /*private void OnTriggerEnter2D(Collider2D other)
+    public Transform bolaSpawnPoint;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Bola"))
         {
-            jogador.PV.Owner.AddScore(1);
+            bola = other.gameObject;
+
+            GolSelect playerGol = GetComponentInParent<GolSelect>();
+
+            playerGol.jogador.PV.Owner.AddScore(1);
             index++;
-
-            if (jogador.PV.Owner.GetScore()index >= LevelManager.Instance.coletaMax)
-            {
-                PhotonNetwork.LocalPlayer.CustomProperties["Ganhador"] = 1;
-                LevelManager.Instance.GoPodium();
-            }
-
-
-            Destroy(gameObject);
-
         }
 
+    }
 
-    }*/
+    IEnumerator ResataBola()
+    {
+        bola.transform.position = bolaSpawnPoint.position;
+
+        yield return new WaitForSeconds(3f);
+
+        StopAllCoroutines();
+    }
 }
---
+++ Design de Games - Inovacao/Assets/GolSelect.cs
@@ -0,0 +1,20 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using Photon.Pun.UtilityScripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolSelect : MonoBehaviour
+{
+    public PlayerMovement jogador;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            jogador = other.GetComponent<PlayerMovement>();
+            this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+}
