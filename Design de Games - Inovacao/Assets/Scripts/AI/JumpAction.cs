05b110fcdab6455058bf7da2a6a113288aab8189
Matias <matiascodas@gmail.com>
2020-04-11 21:00:41 -0300

    Decision

---
+++ Design de Games - Inovacao/Assets/Scripts/AI/JumpAction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Gerador/AI/Action/Jump")]
+public class JumpAction : Actions
+{
+    public override void Act(StateController controller)
+    {
+        Jump(controller);
+    }
+
+    private void Jump(StateController controller)
+    {
+        Debug.Log("Jump");
+
+        controller.rb.AddForce(new Vector2(0, controller.enemyStats.jumpForce), ForceMode2D.Impulse);
+    }
+
+}
