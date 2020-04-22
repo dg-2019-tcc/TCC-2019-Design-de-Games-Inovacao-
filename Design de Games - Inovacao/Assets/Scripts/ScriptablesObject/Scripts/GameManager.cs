f8deb3fb29640643d56062a66d5119e79d08d3f0
Matias <matiascodas@gmail.com>
2020-04-22 11:51:20 -0300

    Futebol

--- Design de Games - Inovacao/Assets/Scripts/ScriptablesObject/Scripts/GameManager.cs
+++ Design de Games - Inovacao/Assets/Scripts/ScriptablesObject/Scripts/GameManager.cs
@@ -14,18 +14,21 @@ namespace Complete
         public AIManager[] m_AI;
         public List<Transform> wayPointsForAI;
 
-		/*
-        private void Start()
+        public GameObject thingsAI;
+
+		
+        /*private void Start()
         {
 			if (OfflineMode.modoDoOffline)
 			{
-				SpawnAI();
+				//SpawnAI();
 			}
-        }
-		*/
+        }*/
+		
 
         public void SpawnAI()
         {
+     
             
                 m_AI[0].m_Instance =
                     Instantiate(aiPrefab[0], m_AI[0].m_SpawnPoint.position, m_AI[0].m_SpawnPoint.rotation) as GameObject;
