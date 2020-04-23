78657f711399c651f7ca3fbbb1e9aede8250a7ff
Matias <matiascodas@gmail.com>
2020-04-22 23:27:43 -0300

    Atualizando os prefabs

--- Design de Games - Inovacao/Assets/Scripts/Interface/Menus/PortaManager.cs
+++ Design de Games - Inovacao/Assets/Scripts/Interface/Menus/PortaManager.cs
@@ -44,7 +44,7 @@ public class PortaManager : MonoBehaviour
         }*/
 	}
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.CompareTag("Player")){
 			switch (qualPorta)
