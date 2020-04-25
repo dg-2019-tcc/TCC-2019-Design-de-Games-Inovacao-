150f3fe56cd224942a0493e4cce2d3237f7fbd20
César Neves Mosca <Cronokami@hotmail.com>
2020-04-24 23:39:28 -0300

    Coletaveis aparecendo aleatoriamente E DE FORMA LEVE, YAY

--- Design de Games - Inovacao/Assets/Scripts/Gameplay/ColetavelGerador.cs
+++ Design de Games - Inovacao/Assets/Scripts/Gameplay/ColetavelGerador.cs
@@ -5,13 +5,42 @@ using UnityEngine;
 public class ColetavelGerador : MonoBehaviour
 {
     public GameObject[] coletaveis;
+	private int index;
 
-    public int indexCole;
-    
-    void Update()
+	private void Start()
+	{
+		for (int i = 0; i < coletaveis.Length; i++)
+		{
+			coletaveis[i].SetActive(false);
+		}
+		index = Random.Range(0, coletaveis.Length);
+		coletaveis[index].SetActive(true);
+	}
+
+
+	void Update()
     {
-        indexCole = DestroyColetavel2D.index;
+		if (coletaveis[index] == null)
+		{
+			pickColetavel();
+		}
+	}
 
-        coletaveis[indexCole].SetActive(true);
-    }
+
+	void pickColetavel()
+	{
+		List<GameObject> tempColetaveis = new List<GameObject>();
+		foreach (GameObject item in coletaveis)
+		{
+			if (item != null)
+				tempColetaveis.Add(item);
+		}
+		coletaveis = tempColetaveis.ToArray();
+		for (int i = 0; i < coletaveis.Length; i++)
+		{
+			coletaveis[i].SetActive(false);
+		}
+		index = Random.Range(0, coletaveis.Length);
+		coletaveis[index].SetActive(true);
+	}
 }
