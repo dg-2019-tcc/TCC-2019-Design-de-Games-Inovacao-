91c8bb405ffb308aa81a7e04cfc01d7bc967094d
César Neves Mosca <Cronokami@hotmail.com>
2020-04-24 22:01:58 -0300

    Timer na tela de vitoria

---
+++ Design de Games - Inovacao/Assets/Scripts/Interface/SendBackHUBTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+public class SendBackHUBTimer : MonoBehaviour
+{
+	public float tempoPraVoltar;
+    // Start is called before the first frame update
+    void Start()
+    {
+		StartCoroutine(TimerToGo(tempoPraVoltar));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+	}
+
+	IEnumerator TimerToGo(float tempo)
+	{
+
+		yield return new WaitForSeconds(tempo);
+		PhotonNetwork.Disconnect();
+		SceneManager.LoadScene("HUB");
+	}
+}
