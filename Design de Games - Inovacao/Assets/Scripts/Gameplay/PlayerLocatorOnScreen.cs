b484c3becb6e059e5110461be66e7ab0c2747a89
César Neves Mosca <Cronokami@hotmail.com>
2020-04-26 21:30:31 -0300

    Locators nos cantos

--- Design de Games - Inovacao/Assets/Scripts/Gameplay/PlayerLocatorOnScreen.cs
+++ Design de Games - Inovacao/Assets/Scripts/Gameplay/PlayerLocatorOnScreen.cs
@@ -35,20 +35,20 @@ public class PlayerLocatorOnScreen : MonoBehaviour
 		{
 			instance.SetActive(true);
 			//Vertical
-			if (instance.transform.position.y >= Screen.height)
+			if (instance.transform.position.y + adjustToScreen >= Screen.height)
 			{
 				instance.transform.position = new Vector3(instance.transform.position.x, Screen.height - adjustToScreen, instance.transform.position.z);
 			}
-			else if (instance.transform.position.y <= 0)
+			else if (instance.transform.position.y - adjustToScreen <= 0)
 			{
 				instance.transform.position = new Vector3(instance.transform.position.x, adjustToScreen, instance.transform.position.z);
 			}
 			//Horizontal
-			if (instance.transform.position.x >= Screen.width)
+			if (instance.transform.position.x + adjustToScreen >= Screen.width)
 			{
 				instance.transform.position = new Vector3(Screen.width - adjustToScreen, instance.transform.position.y, instance.transform.position.z);
 			}
-			else if (instance.transform.position.x <= 0)
+			else if (instance.transform.position.x - adjustToScreen <= 0)
 			{
 				instance.transform.position = new Vector3(adjustToScreen, instance.transform.position.y, instance.transform.position.z);
 			}
