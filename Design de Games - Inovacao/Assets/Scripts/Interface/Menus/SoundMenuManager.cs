1aa72cf6fac9563df7465b0841ae41b7b3a56468
Khalil Yamada HC <khalil.yamada@hotmailcom>
2020-04-27 15:08:18 -0300

    [Áudio / FMOD / HUD] Ajustando protótipo HUD som
    [Áudio / HUD]
     - Finalizei o código dos botões da HUD, agora eu tenho 3 botões (Master, Music e SFX), 
     - Se o master for desligado ou ligado ele afeta os 2 outros botões. 
     - Caso o master esteja desligado e um dos outros 2 botões for apertado, o máster é religado e o botão pressionado também, porém o terceiro botão (que não foi pressionado) não é afetado
    
    [FMOD]
     - Alterei o volume do som do cachorro (SFX/Dog/Dogada) para ficar mais baixo, pois estava muito irritante antes estando tão alto

--- Design de Games - Inovacao/Assets/Scripts/Interface/Menus/SoundMenuManager.cs
+++ Design de Games - Inovacao/Assets/Scripts/Interface/Menus/SoundMenuManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using FMODUnity;
 using FMOD.Studio;
 using UnityEngine.UI;
+using TMPro;
 
 public class SoundMenuManager : MonoBehaviour
 {
@@ -26,11 +27,20 @@ public class SoundMenuManager : MonoBehaviour
     bool isMusicsOn = true;
     bool isSFXOn = true;
 
+    public TextMeshProUGUI masterText;
+    public TextMeshProUGUI musicsText;
+    public TextMeshProUGUI sfxText;
+
+    
+
     void Awake()
     {
         master = RuntimeManager.GetBus("bus:/Master");
         musics = RuntimeManager.GetBus("bus:/Master/Musics");
         sfx = RuntimeManager.GetBus("bus:/Master/SFX");
+        ButtonModeText("Master", isMasterOn, masterText);
+        ButtonModeText("Musics", isMusicsOn, musicsText);
+        ButtonModeText("SFX", isSFXOn, sfxText);
     }
 
     public void SoundMode(Bus myBus, ref bool isSoundOn)
@@ -53,13 +63,64 @@ public class SoundMenuManager : MonoBehaviour
         {
             case "Master":
                 SoundMode(master, ref isMasterOn);
+                if(isMasterOn == false)
+                {
+                    isMusicsOn = false;
+                    musics.setMute(true);
+                    isSFXOn = false;
+                    sfx.setMute(true);
+                }
+                else
+                {
+                    
+                    isMusicsOn = false;
+                    isSFXOn = false;
+                    SoundMode(musics, ref isMusicsOn);
+                    SoundMode(sfx, ref isSFXOn);
+                    
+                }
+                ButtonModeText("Master", isMasterOn, masterText);
+                ButtonModeText("Musics", isMusicsOn, musicsText);
+                ButtonModeText("SFX", isSFXOn, sfxText);
                 break;
+
+
             case "Musics":
                 SoundMode(musics, ref isMusicsOn);
+                if (isMusicsOn == true && isMasterOn == false)
+                {
+                    SoundMode(master, ref isMasterOn);
+                    ButtonModeText("Master", isMasterOn, masterText);
+                }
+                ButtonModeText("Musics", isMusicsOn, musicsText);
                 break;
+
+
             case "SFX":
-                SoundMode(sfx, ref isSFXOn);
+                SoundMode(sfx, ref isSFXOn); 
+                if (isSFXOn == true && isMasterOn == false)
+                {
+                    SoundMode(master, ref isMasterOn);
+                    ButtonModeText("Master", isMasterOn, masterText);
+                }
+                ButtonModeText("SFX", isSFXOn, sfxText);
                 break;
         }
     }
+
+
+    public void ButtonModeText(string buttonText, bool isOn, TextMeshProUGUI textSpace)
+    {
+        if(isOn == true)
+        {
+            textSpace.text = buttonText + ": On";
+            textSpace.color = Color.green;
+        }
+        else
+        {
+            textSpace.text = buttonText + ": Off";
+            textSpace.color = Color.red;
+        }
+    }
 }
+
