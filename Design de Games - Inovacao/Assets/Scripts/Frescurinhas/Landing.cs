e454b8e806e81d4fa49426230fba57ce595ee4ce
César Neves Mosca <Cronokami@hotmail.com>
2020-04-28 00:02:57 -0300

    Particulas de quando cai no chão

---
+++ Design de Games - Inovacao/Assets/Scripts/Frescurinhas/Landing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Landing : MonoBehaviour
+{
+	private Player2DAnimations p2Danim;
+	private string oldAnimState;
+	private ParticleSystem particle;
+    // Start is called before the first frame update
+    void Start()
+    {
+		particle = GetComponent<ParticleSystem>();
+		p2Danim = GetComponentInParent<Player2DAnimations>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (p2Danim.state.ToString() == "Aterrisando" && oldAnimState != p2Danim.state.ToString())
+		{
+
+			particle.Play();
+		}
+
+		oldAnimState = p2Danim.state.ToString();
+    }
+}
