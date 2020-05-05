19216aa33da6ac8ce4af497a52344d0b9a40ce77
César Neves Mosca <Cronokami@hotmail.com>
2020-05-05 02:32:03 -0300

    Coletavel da moto pegavel e bonitinho

---
+++ Design de Games - Inovacao/Assets/Scripts/Corrida/MotoMoedinhas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotoMoedinhas : MonoBehaviour
+{
+
+	public ParticleSystem brilhinho;
+
+	public void Coleta()
+	{
+		brilhinho.loop = false;
+		brilhinho.startSpeed = 1;
+		
+	}
+}
