fd460c181ad563a47cd19c228a3bc355b6960bdc
Matias <matiascodas@gmail.com>
2020-05-05 12:37:23 -0300

    Colocando o cachorro certo

---
+++ Design de Games - Inovacao/Assets/Scripts/DogAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DragonBones;
+
+public class DogAnimation : MonoBehaviour
+{
+    public string idlePose = "0_Idle";
+    [SerializeField]
+    private UnityArmatureComponent dog;
+
+    public BoolVariable dogAtivo;
+    public BoolVariable carroActive;
+    public BoolVariable pipaActive;
+
+    public enum State { Idle, Pipa, Carrinho, Moto, Aviao}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Idle()
+    {
+
+    }
+
+    public void Pipa()
+    {
+
+    }
+
+    public void Carrinho()
+    {
+
+    }
+
+
+}
