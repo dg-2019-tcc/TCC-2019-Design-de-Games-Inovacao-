201a4081ecb77fab4b7f009f6a6554a38591a01b
César Neves Mosca <Cronokami@hotmail.com>
2020-05-05 21:17:27 -0300

    Card da coleta e do futebol aparecendo só primeira vez e na tela de vit

--- Design de Games - Inovacao/Assets/Scripts/Interface/SendBackHUBTimer.cs
+++ Design de Games - Inovacao/Assets/Scripts/Interface/SendBackHUBTimer.cs
@@ -7,23 +7,58 @@ using Photon.Pun;
 public class SendBackHUBTimer : MonoBehaviour
 {
 	public float tempoPraVoltar;
-    // Start is called before the first frame update
+
     void Start()
     {
 		StartCoroutine(TimerToGo(tempoPraVoltar));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-		
-	}
+   
 
 	IEnumerator TimerToGo(float tempo)
 	{
 
 		yield return new WaitForSeconds(tempo);
 		PhotonNetwork.Disconnect();
-		SceneManager.LoadScene("HUB");
+		ChooseScene();
+		
 	}
+
+	private void ChooseScene()
+	{
+		switch (HistoryChecker.sceneName)
+		{
+			case "Coleta":
+				if (PlayerPrefs.GetInt("PrimeiraVezNaColeta") == 1)
+				{
+					PlayerPrefs.SetInt("PrimeiraVezNaColeta", 1);
+					SceneManager.LoadScene("HUB");
+				}
+				else
+				{
+					HistoryChecker.sceneName = null;
+					SceneManager.LoadScene("HistoriaColeta");
+				}
+				break;
+
+			case "Futebol":
+				if (PlayerPrefs.GetInt("PrimeiraVezNoFutebol") == 1)
+				{
+					PlayerPrefs.SetInt("PrimeiraVezNoFutebol", 1);
+					SceneManager.LoadScene("HUB");
+				}
+				else
+				{
+					HistoryChecker.sceneName = null;
+					SceneManager.LoadScene("HistoriaFutebol");
+				}
+				break;
+			
+			default:
+				SceneManager.LoadScene("HUB");
+				break;
+		}
+		
+	}
+
 }
