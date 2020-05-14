43c249597ce11958e7c088af2613a1e400deffcf
César Neves Mosca <Cronokami@hotmail.com>
2020-05-13 22:57:32 -0300

    Tutorial com chãos e câmeras funcionando

---
+++ Design de Games - Inovacao/Assets/Scripts/Tutorial/ChaoQueCai.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaoQueCai : MonoBehaviour
+{
+	private bool playerTocando;
+	public GameObject coletavel;
+
+
+	private void Update()
+	{
+		if (playerTocando && coletavel == null)
+		{
+			Destroy(gameObject);
+		}
+
+		playerTocando = false;
+	}
+
+	public void ToAqui()
+	{
+		playerTocando = true;
+
+	}
+}
---
+++ Design de Games - Inovacao/Assets/Scripts/Tutorial/UnsetMidpoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnsetMidpoint : MonoBehaviour
+{
+	CamLookaheadAim script;
+
+
+    void Update()
+    {
+		if (script == null)
+		{
+			script = FindObjectOfType<CamLookaheadAim>();
+			script.deltaAltura = 0.03f;
+		}
+    }
+
+}
