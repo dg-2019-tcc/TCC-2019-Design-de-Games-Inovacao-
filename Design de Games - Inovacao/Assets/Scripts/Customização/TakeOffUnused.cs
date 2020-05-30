dd1e79cb6d7633f534668c7b61466b2b6ba48e3f
César Neves Mosca <Cronokami@hotmail.com>
2020-05-30 06:17:38 -0300

    Desativando peças não usadas durante o jogo

---
+++ Design de Games - Inovacao/Assets/Scripts/Customização/TakeOffUnused.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeOffUnused : MonoBehaviour
+{
+	public bool letThemBeOn = false;
+
+	private void Start()
+	{
+		CheckAndExecute();
+	}
+
+	private void Update()
+	{
+		CheckAndExecute();
+		letThemBeOn = true;
+	}
+
+	private void CheckAndExecute()
+	{
+		if (letThemBeOn)
+		{
+			return;
+
+		}
+
+		foreach (Transform part in GetComponentsInChildren<Transform>(true))
+		{
+			ChangeMultipleCustom changeMultipleCustom = part.GetComponent<ChangeMultipleCustom>();
+			if (!part.gameObject.activeSelf && changeMultipleCustom != null)
+			{
+				foreach  (GameObject subPart in changeMultipleCustom.multipleCustom)
+				{
+					Destroy(subPart);
+				}
+			}
+		}
+	}
+}
