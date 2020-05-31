09d840ca7a74e3ca1a4c39358a3caa90a3021d48
César Neves Mosca <Cronokami@hotmail.com>
2020-05-31 18:00:30 -0300

    Particula da moto agr na frente

---
+++ Design de Games - Inovacao/Assets/Scripts/Frescurinhas/TrailDirection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailDirection : MonoBehaviour
+{
+	private Vector3 oldPos;
+
+    
+    void Update()
+    {
+		transform.right = transform.position - oldPos;
+		oldPos = transform.position;
+    }
+}
