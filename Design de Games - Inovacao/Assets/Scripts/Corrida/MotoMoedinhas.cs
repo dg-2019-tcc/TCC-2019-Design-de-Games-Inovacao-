777dcce6707faf61c2350ea4e08623b7191f150b
Khalil Yamada HC <khalil.yamada@hotmailcom>
2020-06-03 01:35:42 -0300

    [Feedback] Moedinha moto com feedback

--- Design de Games - Inovacao/Assets/Scripts/Corrida/MotoMoedinhas.cs
+++ Design de Games - Inovacao/Assets/Scripts/Corrida/MotoMoedinhas.cs
@@ -7,11 +7,18 @@ public class MotoMoedinhas : MonoBehaviour
 
 	public ParticleSystem brilhinho;
 
+	bool coletada = true;
+
 	public void Coleta()
 	{
 		brilhinho.loop = false;
 		brilhinho.startSpeed = 5;
 		brilhinho.emissionRate = 100;
-		
+		if (coletada)
+		{
+			FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Feedback/Coletaveis/ColetaMoeda");
+			coletada = false;
+		}
+
 	}
 }
