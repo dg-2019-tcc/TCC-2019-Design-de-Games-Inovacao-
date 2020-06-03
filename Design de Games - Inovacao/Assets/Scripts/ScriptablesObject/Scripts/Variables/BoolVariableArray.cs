d462c89b4c65d55c8208e6290e3dc555ac376491
César Neves Mosca <Cronokami@hotmail.com>
2020-06-03 13:35:54 -0300

    Imagens do mc colocadas na cena(ainda a serem ativadas)

---
+++ Design de Games - Inovacao/Assets/Scripts/ScriptablesObject/Scripts/Variables/BoolVariableArray.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Gerador/Variables/BoolArray")]
+public class BoolVariableArray : ScriptableObject
+{
+	public bool[] Value;
+}
