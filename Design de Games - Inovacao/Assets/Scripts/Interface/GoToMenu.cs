62a539484e43743ae919b37de47d13739a963f7a
César Neves Mosca <Cronokami@hotmail.com>
2020-06-08 18:48:28 -0300

    FirstScene indo pra próxima cena

---
+++ Design de Games - Inovacao/Assets/Scripts/Interface/GoToMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMenu : MonoBehaviour
+{
+    void Start()
+    {
+		StartCoroutine(WaitAndGo());
+    }
+
+	private IEnumerator WaitAndGo()
+	{
+		yield return new WaitForSeconds(0.5f);
+		SceneManager.LoadScene(1);
+	}
+    
+}
