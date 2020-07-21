d25f200d3a94235686751783274a27a2d06e5d21
César Neves Mosca <Cronokami@hotmail.com>
2020-07-21 10:27:37 -0300

    Feedback pro jogador das moedas(bruto, falta conversar com grupo)

---
+++ Design de Games - Inovacao/Assets/Scripts/Frescurinhas/MoedaFeedbackLerp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoedaFeedbackLerp : MonoBehaviour
+{
+	public TMP_Text texto;
+	public Points moedas;
+	private float pontosDisplay;
+	/*
+    void Start()
+    {
+		texto = GetComponent<TMP_Text>();
+    }
+	*/
+
+    void Update()
+    {
+		pontosDisplay = Mathf.Lerp(pontosDisplay, moedas.Value, 0.5f);
+		texto.text = pontosDisplay.ToString();
+    }
+}
--- Design de Games - Inovacao/Assets/Scripts/ScriptableObjects/Scripts/Player/Points.cs
+++ Design de Games - Inovacao/Assets/Scripts/ScriptableObjects/Scripts/Player/Points.cs
@@ -7,9 +7,24 @@ public class Points : ScriptableObject
 {
 	public float Value;
 
+	public GameObject feedbackCanvas;
+
+	public float waitTimeToLetAddAgain;
+
+	[Header ("Debug only")]
+	public float lastTimeThisRan = 0;
 	public void Add(float points)
 	{
+		if (stopAdding()) return;
 		Value += points;
 		PlayerPrefs.SetFloat("Moedas", Value);
+		Instantiate(feedbackCanvas);
+		lastTimeThisRan = Time.time;
+	}
+
+	
+	private bool stopAdding()
+	{
+		return Time.time - waitTimeToLetAddAgain <= lastTimeThisRan;
 	}
 }
