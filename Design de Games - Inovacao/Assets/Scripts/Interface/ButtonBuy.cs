8b56583a33ea73168cf3c9cc312ffa2643422257
César Neves Mosca <Cronokami@hotmail.com>
2020-08-24 18:25:59 -0300

    Botão de comprar itens(e correções em certas cenas)

---
+++ Design de Games - Inovacao/Assets/Scripts/Interface/ButtonBuy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBuy : MonoBehaviour
+{
+	public int x;
+	public int y;
+
+	public BoolVariableMatrix blocked;
+
+	void QualX(int value)
+	{
+		x = value;
+	}
+
+	void QualY(int value)
+	{
+		y = value;
+	}
+
+
+	public void LiberaCustom()
+	{
+		blocked.rows[x].row[y] = false;
+		Destroy(gameObject);
+	}
+}
