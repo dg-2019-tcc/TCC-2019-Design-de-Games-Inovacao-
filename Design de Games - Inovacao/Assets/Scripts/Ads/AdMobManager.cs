7477e023ccd280d4a21f0588e7ca288637eb03a1
César Neves Mosca <Cronokami@hotmail.com>
2020-08-31 14:40:37 -0300

    Ads teste do google(mudar ID pra versão final)
    no script AdMobManager tem o id teste, devemos mudar para a versão final

---
+++ Design de Games - Inovacao/Assets/Scripts/Ads/AdMobManager.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+using GoogleMobileAds.Api;
+using GoogleMobileAds.Common;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+
+public class AdMobManager : MonoBehaviour
+{
+
+	public static AdMobManager instance;
+	
+	private RewardedAd rewardedAd;
+
+
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else if(instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	void Start()
+    {
+		MobileAds.Initialize(initStatus => { });
+
+
+
+		string adUnitId;
+#if UNITY_ANDROID
+		adUnitId = "ca-app-pub-3940256099942544/5224354917";
+#elif UNITY_IPHONE
+            adUnitId = "ca-app-pub-3940256099942544/1712485313";
+#else
+            adUnitId = "unexpected_platform";
+#endif
+
+		this.rewardedAd = new RewardedAd(adUnitId);
+
+		// Called when an ad request has successfully loaded.
+		this.rewardedAd.OnAdLoaded += HandleRewardedAdLoaded;
+		// Called when an ad request failed to load.
+		this.rewardedAd.OnAdFailedToLoad += HandleRewardedAdFailedToLoad;
+		// Called when an ad is shown.
+		this.rewardedAd.OnAdOpening += HandleRewardedAdOpening;
+		// Called when an ad request failed to show.
+		this.rewardedAd.OnAdFailedToShow += HandleRewardedAdFailedToShow;
+		// Called when the user should be rewarded for interacting with the ad.
+		this.rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
+		// Called when the ad is closed.
+		this.rewardedAd.OnAdClosed += HandleRewardedAdClosed;
+
+		// Create an empty ad request.
+		AdRequest request = new AdRequest.Builder().Build();
+		// Load the rewarded ad with the request.
+		this.rewardedAd.LoadAd(request);
+	}
+
+
+	public void HandleRewardedAdLoaded(object sender, EventArgs args)
+	{
+		MonoBehaviour.print("HandleRewardedAdLoaded event received");
+	}
+
+	public void HandleRewardedAdFailedToLoad(object sender, AdErrorEventArgs args)
+	{
+		MonoBehaviour.print(
+			"HandleRewardedAdFailedToLoad event received with message: "
+							 + args.Message);
+	}
+
+	public void HandleRewardedAdOpening(object sender, EventArgs args)
+	{
+		MonoBehaviour.print("HandleRewardedAdOpening event received");
+	}
+
+	public void HandleRewardedAdFailedToShow(object sender, AdErrorEventArgs args)
+	{
+		MonoBehaviour.print(
+			"HandleRewardedAdFailedToShow event received with message: "
+							 + args.Message);
+	}
+
+	public void HandleRewardedAdClosed(object sender, EventArgs args)
+	{
+		MonoBehaviour.print("HandleRewardedAdClosed event received");
+	}
+
+	public void HandleUserEarnedReward(object sender, Reward args)
+	{
+		string type = args.Type;
+		double amount = args.Amount;
+		MonoBehaviour.print(
+			"HandleRewardedAdRewarded event received for "
+						+ amount.ToString() + " " + type);
+	}
+
+
+
+	public void UserChoseToWatchAd()
+	{
+		if (this.rewardedAd.IsLoaded())
+		{
+			this.rewardedAd.Show();
+			Debug.Log("Ad is being shown");
+		}
+	}
+
+
+}
---
+++ Design de Games - Inovacao/Assets/Scripts/TesteAd.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesteAd : MonoBehaviour
+{
+
+	public void ShowAd()
+	{
+		AdMobManager.instance.UserChoseToWatchAd();
+		Debug.Log("User chose to watch Ad");
+
+	}
+}
