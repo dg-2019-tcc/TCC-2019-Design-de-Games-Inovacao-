44f34a25d15f702ff649be45c1b768dcb84e0c51
César Neves Mosca <Cronokami@hotmail.com>
2020-09-11 17:11:04 -0300

    Propagandas recompensadas nas customizações e obrig. no final de fases

---
+++ Design de Games - Inovacao/Assets/Scripts/Ads/RewardedAdMoedas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class RewardedAdMoedas : MonoBehaviour
+{
+
+	private TMP_Text text;
+	private Button button;
+
+
+    void Start()
+    {
+		button = GetComponent<Button>();
+		text = GetComponentInChildren<TMP_Text>();
+    }
+	
+    void Update()
+    {
+
+		button.interactable = AdMobManager.instance.rewardedAd.IsLoaded();
+	}
+
+
+	void ShowAd()
+	{
+		AdMobManager.instance.UserChoseToWatchAd();
+	}
+}
